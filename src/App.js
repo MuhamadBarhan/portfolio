edffa7e03cd08fa0c9e311b0d2e465b94a843271
unknown <muhammadfarhan3120@gmail.com>
2024-05-28 20:14:44 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Intro from "./components/Intro/intro";
 import About from "./components/About/about";
 import Skills from "./components/Skills/skills";
 import Projects from "./components/Projects/Projects";
+import Certifications from "./components/Certifications/Certifications";
 import Contact from "./components/Contact/contact";
 import Footer from "./components/Footer/footer";
 
@@ -15,6 +16,7 @@ function App() {
       <About/>
       <Skills/>
       <Projects/>
+      <Certifications/>
       <Contact/>
       <Footer/>
     </div>
---
+++ src/components/Certifications/Certifications.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Certifications.css'
+
+const Certifications = () => {
+  return (
+    <section id="certifications">
+        <span className="cert-heading">Certifications</span>
+        <div className="certificate">
+        <iframe src="https://www.hackerrank.com/certificates/iframe/dd8fed1f4529" title="Embedded Document" width="600" height="400"></iframe>
+        </div>
+    </section>
+  )
+}
+
+export default Certifications
