284f826e85a31586ac7912a5cdd1ce411c07783a
unknown <muhammadfarhan3120@gmail.com>
2024-06-02 17:38:02 +0530

    Changes

--- src/components/Certifications/Certifications.js
+++ src/components/Certifications/Certifications.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import './Certifications.css'
+import c1 from '../../assets/react.jpg'
+import c2 from '../../assets/tcs.jpg'
+import c3 from '../../assets/sql.jpg'
+import c4 from '../../assets/ibm.jpg'
+
 
 const Certifications = () => {
   return (
     <section id="certifications">
         <span className="cert-heading">Certifications</span>
-        <div className="certificate">
-        <iframe src="https://www.hackerrank.com/certificates/iframe/dd8fed1f4529" title="Embedded Document" width="600" height="400"></iframe>
+        <div className="certificates">
+        <img src={c1} alt="" className='certificate'/>
+        <img src={c2} alt="" className='certificate'/>
+        <img src={c3} alt="" className='certificate'/>
+        <img src={c4} alt="" className='certificate'/>
         </div>
     </section>
   )
