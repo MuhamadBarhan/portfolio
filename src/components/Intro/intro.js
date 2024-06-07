e0f3c203c24f3c92018ec5f907e1e41b8a52193d
unknown <muhammadfarhan3120@gmail.com>
2024-06-07 18:23:22 +0530

    Changes

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -2,25 +2,36 @@ import React from 'react'
 import './intro.css'
 import { useInView } from 'react-intersection-observer';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faLinkedinIn ,faGithub, faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faLinkedinIn, faGithub, faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { TypeAnimation } from 'react-type-animation';
 
 const Intro = () => {
     const [ref, inView] = useInView({
         triggerOnce: false,
-      });
-    
+    });
+
 
     return (
         <section id='intro'>
             <div ref={ref} className={`introContent ${inView ? 'animate' : ''}`}>
                 <span className="hello">HELLO!</span>
                 <span className="introText">I'm <span className="introName">Muhamad Barhan </span></span>
-                <span className="introTitle">Web Designer</span>
-                <p className="introPara">I am a skilled web designer with experience in creating visually <br/>appealing and user friendly websites</p>
+                    <TypeAnimation
+                        sequence={[
+                            'Full Stack Developer', 2000,
+                            'MERN Stack', 2000,
+                            'UI/UX Designer', 2000,
+                        ]}
+                        wrapper="span"
+                        cursor={true}
+                        repeat={Infinity}
+                        className="introTitle"
+                    />
+                <p className="introPara">I am a skilled web designer with experience in creating visually <br />appealing and user friendly websites</p>
                 <div className="contact-container">
-                    <a className='contact-icon' href='https://www.linkedin.com/in/muhamad-barhan-h/'><FontAwesomeIcon icon={faLinkedinIn} className='fa-icon'/></a>
-                    <a className='contact-icon' href='https://github.com/MuhamadBarhan'><FontAwesomeIcon icon={faGithub} className='fa-icon'/></a>
-                    <a className='contact-icon' href='https://instagram.com/farhan__mhd__'><FontAwesomeIcon icon={faInstagram} className='fa-icon'/></a>
+                    <a className='contact-icon' href='https://www.linkedin.com/in/muhamad-barhan-h/'><FontAwesomeIcon icon={faLinkedinIn} className='fa-icon' /></a>
+                    <a className='contact-icon' href='https://github.com/MuhamadBarhan'><FontAwesomeIcon icon={faGithub} className='fa-icon' /></a>
+                    <a className='contact-icon' href='https://instagram.com/farhan__mhd__'><FontAwesomeIcon icon={faInstagram} className='fa-icon' /></a>
                 </div>
             </div>
         </section>
