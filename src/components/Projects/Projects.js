e33296188404f4beeeb39bdb942928019bd1d832
MuhamadBarhan <muhammadfarhan3120@gmail.com>
2024-07-04 12:14:56 +0530

    Changes

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -6,33 +6,55 @@ import ecomImg from '../../assets/ecom.PNG';
 const Projects = () => {
   return (
     <section id="projects">
-      <span className="projects-heading">Projects</span>
-      <p className='projects-desc'></p>
+      <h2 className="projects-heading">Projects</h2>
       <div className="project-container">
         <div className="project">
-          <img src={ecomImg} alt="" />
+          <img src={ecomImg} alt="ECommerce Website" className="project-image" />
           <div className="project-details">
-            <p className='project-title'>ECommerce Website-MERN Stack</p>
-            <p className='project-desc'>Devloped a dynamic ecommerce website using MERN stack(MongoDB,Express.js,React.js,Node.js)</p>
-            <a href='https://mhd-ecommerce.vercel.app/' className='project-btn'>Demo</a>   
-            <a href='https://github.com/MuhamadBarhan/Ecommerce-Website-using-MERN-stack' className='project-btn'>View Code</a>
+            <p className='project-title'>ECommerce Website - MERN Stack</p>
+            <p className='project-desc'>Developed a dynamic ecommerce website using MERN stack (MongoDB, Express.js, React.js, Node.js).</p>
+            <div className='tech-stacks'>
+              <span>ReactJS</span>
+              <span>NodeJS</span>
+              <span>ExpressJS</span>
+              <span>MongoDB</span>
+              <span>Redux</span>
+            </div>
+            <div className="project-buttons">
+              <a href='https://mhd-ecommerce.vercel.app/' className='project-btn'>Demo</a>   
+              <a href='https://github.com/MuhamadBarhan/Ecommerce-Website-using-MERN-stack' className='project-btn'>View Code</a>
+            </div>
           </div>
         </div>
         <div className="project">
-          <img src={movieImg} alt="" />
+          <img src={movieImg} alt="Movie Ticket Booking" className="project-image" />
           <div className="project-details">
             <p className='project-title'>Movie Ticket Booking</p>
-            <p className='project-desc'>Developed frontend using react.js and the website is responsive across devices</p>
-            <a href='https://mhd-ecommerce.vercel.app/' className='project-btn'>Demo</a>   
-            <a href='https://github.com/MuhamadBarhan/Ecommerce-Website-using-MERN-stack' className='project-btn'>View Code</a>
+            <p className='project-desc'>Developed frontend using React.js and the website is responsive across devices.</p>
+            <div className='tech-stacks'>
+              <span>ReactJS</span>
+              <span>CSS</span>
+              <span>JavaScript</span>
+            </div>
+            <div className="project-buttons">
+              <a href='https://mhd-ecommerce.vercel.app/' className='project-btn'>Demo</a>   
+              <a href='https://github.com/MuhamadBarhan/Ecommerce-Website-using-MERN-stack' className='project-btn'>View Code</a>
+            </div>
           </div>
         </div>
         <div className="project">
           <div className="project-details">
             <p className='project-title'>Trash Management - Admin Panel</p>
-            <p className='project-desc'>Developed an admin panel that displays the data from firebase</p>
-            <a href='' className='project-btn'>Demo</a>   
-            <a href='' className='project-btn'>View Code</a>
+            <p className='project-desc'>Developed an admin panel that displays the data from Firebase.</p>
+            <div className='tech-stacks'>
+              <span>ReactJS</span>
+              <span>Firebase</span>
+              <span>Material-UI</span>
+            </div>
+            <div className="project-buttons">
+              <a href='#' className='project-btn'>Demo</a>   
+              <a href='#' className='project-btn'>View Code</a>
+            </div>
           </div>
         </div>
       </div>
