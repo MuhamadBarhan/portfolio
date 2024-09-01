bacf4669dc5e57cca471b1cdee8d20cbdef2cca0
MuhamadBarhan <muhammadfarhan3120@gmail.com>
2024-09-01 20:21:19 +0530

    Changes

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -68,10 +68,9 @@ const Projects = () => {
             <p className='t1'>Dec 2023</p>
             <p className='project-desc'>Developed a cross-platform Tourism App using Flutter, integrating Google Maps API for interactive navigation. The app provides rich travel destination details with a user-friendly interface. Focused on optimizing performance for a seamless experience on both Android and iOS.</p>
             <div className='tech-stacks'>
-              <span>#HTML</span>
-              <span>#CSS</span>
-              <span>#JavaScript</span>
-              <span>#Firebase</span>
+              <span>#Flutter</span>
+              <span>#Dart</span>
+              <span>#GoogleMap API</span>
             </div>
             <div className="project-buttons">
               <a href='#' className='project-btn'>Demo</a>   
@@ -89,6 +88,21 @@ const Projects = () => {
               <span>#CSS</span>
               <span>#JavaScript</span>
               <span>#Firebase</span>
+            </div>
+            <div className="project-buttons">
+              <a href='#' className='project-btn'>Demo</a>   
+              <a href='https://github.com/MuhamadBarhan/Trash-Management-Admin-Panel' className='project-btn'>View Code</a>
+            </div>
+          </div>
+        </div>
+        <div className="project">
+          <div className="project-details">
+            <p className='project-title'>Facial Expression Recognition using CNN</p>
+            <p className='t1'>May 2023</p>
+            <p className='project-desc'>Facial Expression Recognition using CNN" is a project that utilizes Convolutional Neural Networks to accurately identify and classify human emotions based on facial expressions. The system processes images to detect subtle differences in facial features, enabling real-time emotion analysis</p>
+            <div className='tech-stacks'>
+              <span>#Python</span>
+              <span>#CNN</span>
             </div>
             <div className="project-buttons">
               <a href='#' className='project-btn'>Demo</a>   
