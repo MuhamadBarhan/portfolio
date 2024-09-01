cb4b8129ade68f56e7bd09925875656463cfd4e0
MuhamadBarhan <muhammadfarhan3120@gmail.com>
2024-09-01 16:18:10 +0530

    Changes

--- src/components/Certifications/Certifications.js
+++ src/components/Certifications/Certifications.js
@@ -1,23 +1,50 @@
-import React from 'react'
-import './Certifications.css'
-import c1 from '../../assets/react.jpg'
-import c2 from '../../assets/tcs.jpg'
-import c3 from '../../assets/sql.jpg'
-import c4 from '../../assets/ibm.jpg'
-
+import React from 'react';
+import './Certifications.css';
+import certreact from '../../assets/react.jpg';
+import certtcs from '../../assets/tcs.jpg';
+import certsql from '../../assets/sql.jpg';
+import certibm from '../../assets/ibm.jpg';
+import certcyber from '../../assets/udemy.jpg';
+import certdocker from '../../assets/docker.jpg';
+import certaptitude from '../../assets/aptitude.jpg';
 
 const Certifications = () => {
   return (
     <section id="certifications">
-        <span className="cert-heading">Certifications</span>
-        <div className="certificates">
-        <img src={c1} alt="" className='certificate'/>
-        <img src={c2} alt="" className='certificate'/>
-        <img src={c3} alt="" className='certificate'/>
-        <img src={c4} alt="" className='certificate'/>
+      <span className="cert-heading">Certifications</span>
+
+      <div className="certificate-container">
+        <div className="certificate">
+          <p>React JS Certification</p>
+          <a href={certreact} target="_blank" rel="noopener noreferrer">View</a>
         </div>
+        <div className="certificate">
+          <p>TCS iON Certification</p>
+          <a href={certtcs} target="_blank" rel="noopener noreferrer">View</a>
+        </div>
+        <div className="certificate">
+          <p>SQL Certification</p>
+          <a href={certsql} target="_blank" rel="noopener noreferrer">View</a>
+        </div>
+        <div className="certificate">
+          <p>IBM Certification</p>
+          <a href={certibm} target="_blank" rel="noopener noreferrer">View</a>
+        </div>
+        <div className="certificate">
+          <p>Cyber Security - Udemy</p>
+          <a href={certcyber} target="_blank" rel="noopener noreferrer">View</a>
+        </div>
+        <div className="certificate">
+          <p>Docker Certification</p>
+          <a href={certdocker} target="_blank" rel="noopener noreferrer">View</a>
+        </div>
+        <div className="certificate">
+          <p>Aptitude Certification</p>
+          <a href={certaptitude} target="_blank" rel="noopener noreferrer">View</a>
+        </div>
+      </div>
     </section>
-  )
+  );
 }
 
-export default Certifications
+export default Certifications;
